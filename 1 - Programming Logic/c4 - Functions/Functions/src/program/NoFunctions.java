a46e0e0f4e8a125809de0dec90bf53732c4c7f01
Beatriz Almeida <beatriz.06.06.2016@gmail.com>
2025-09-05 10:53:35 -0300

    :sparkles: Organizing files

--- 1 - Programming Logic/c4 - Functions/Functions/src/program/NoFunctions.java
+++ 1 - Programming Logic/c4 - Functions/Functions/src/program/NoFunctions.java
@@ -11,14 +11,14 @@ public class NoFunctions {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Type three numbers and we will check which is the highest.");
-		
+
 		System.out.println("The #1: ");
 		int number1 = sc.nextInt();
 		System.out.println("The #2: ");
 		int number2 = sc.nextInt();
 		System.out.println("The #3: ");
 		int number3 = sc.nextInt();
-		
+
 		int highest = number1;
 
 		if (number2 > highest)
