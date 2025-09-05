88739aa27c1746faf1df09d9b00ff69e3b8c7f4b
Beatriz Almeida <beatriz.06.06.2016@gmail.com>
2025-09-05 11:40:08 -0300

    :sparkles: Add code extra exercise

---
+++ 1 - Programming Logic/d - Exercise/project/src/program/Main.java
@@ -0,0 +1,37 @@
+package program;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("How many names will you register?");
+		int quatityUsers = sc.nextInt();
+		
+		System.out.println("Type the names separated with a comma (,):");
+		sc.nextLine();
+		String users = sc.nextLine();
+		
+		String[] vect = users.split(",");
+		
+		for (int i=0; i<quatityUsers; i++) {
+			String name = vect[i];
+			
+			System.out.print("Name: " + name + " | ");
+			
+			int size = name.length();
+			System.out.print("Chars: " + size + " | ");
+			
+			if (size <= 5) {
+				System.out.printf("Short name%n");
+			}
+			else {
+				System.out.printf("Long name%n");
+			}
+		}
+		sc.close();
+	}
+}
