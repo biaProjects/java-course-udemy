1b55efe86194a758c1511badeabb460d30902de2
Beatriz Almeida <beatriz.06.06.2016@gmail.com>
2025-09-05 12:04:27 -0300

    :sparkles: Organizing files

---
+++ 1 - Programming Logic/c4 - Functions/project/src/program/NoFunctions.java
@@ -0,0 +1,33 @@
+// let's do a program that read 3 numbers and print the highest
+
+package program;
+
+import java.util.Scanner;
+
+public class NoFunctions {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Type three numbers and we will check which is the highest.");
+
+		System.out.println("The #1: ");
+		int number1 = sc.nextInt();
+		System.out.println("The #2: ");
+		int number2 = sc.nextInt();
+		System.out.println("The #3: ");
+		int number3 = sc.nextInt();
+
+		int highest = number1;
+
+		if (number2 > highest)
+			highest = number2;
+		if (number3 > highest)
+			highest = number3;
+
+		System.out.println("Highest: " + highest);
+
+		sc.close();
+	}
+}
---
+++ 1 - Programming Logic/c4 - Functions/project/src/program/WithFunctions.java
@@ -0,0 +1,46 @@
+// let's do a program that read 3 numbers and print the highest
+
+package program;
+
+import java.util.Scanner;
+
+public class WithFunctions {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Type three numbers and we will check which is the highest.");
+
+		System.out.println("The #1: ");
+		int number1 = sc.nextInt();
+		System.out.println("The #2: ");
+		int number2 = sc.nextInt();
+		System.out.println("The #3: ");
+		int number3 = sc.nextInt();
+
+		// let's use our functions:
+		int higher = max(number1, number2, number3);
+
+		showResult(higher);
+
+		sc.close();
+	}
+
+	public static int max(int x, int y, int z) {
+		int aux; // local variable (it only exists in this function scope - and so is x, y, z)
+		if (x > y && x > z) {
+			aux = x;
+		} else if (y > z) {
+			aux = y;
+		} else {
+			aux = z;
+		}
+		return aux;
+	}
+
+	// let's create another one, just to print the result
+	public static void showResult(int value) {
+		System.out.println("Higher = " + value);
+	}
+}
