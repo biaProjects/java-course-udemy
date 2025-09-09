83da87a5e5a85064b66e927f4cf4aeb24c9ca8fb
Beatriz Almeida <beatriz.06.06.2016@gmail.com>
2025-09-08 22:09:09 -0300

    Add exercise 2

--- 2 - Object-Oriented Programming - Part1/a - Introduction to OOP/c - Exercises/Exercise-1 rectangle/src/application/Program.java
+++ 2 - Object-Oriented Programming - Part1/a - Introduction to OOP/c - Exercises/Exercise-1 rectangle/src/application/Program.java
@@ -26,6 +26,7 @@ public class Program {
 		sc.close();
 	}
 }
+
 /*
 |       Rectangle        |
 | ---------------------- |
---
+++ 2 - Object-Oriented Programming - Part1/a - Introduction to OOP/c - Exercises/Exercise-2 salary/src/application/Program.java
@@ -0,0 +1,52 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Please, inform the following informations od the employee:");		
+		Employee employee = new Employee();
+		
+		System.out.print("Name: ");
+		employee.name = sc.nextLine();
+		
+		System.out.print("Gross salary: ");
+		employee.grossSalary = sc.nextDouble();
+
+		System.out.print("Tax: ");
+		employee.tax = sc.nextDouble();
+		
+		System.out.println();
+		
+		System.out.println("Employee: " + employee);
+		System.out.println();
+		
+		System.out.print("Which percentage to increase salary of the employee? ");
+		double percentage = sc.nextDouble();
+		employee.increaseSalary(percentage);
+		
+		System.out.println();
+		
+		System.out.println("Updated data: " + employee);
+		
+		sc.close();
+	}
+}
+/*
+|                   Employee                   |
+| -------------------------------------------- |
+|                - Name : string               |
+|            - GrossSalary : double            |
+|                - Tax : double                |
+| -------------------------------------------- |
+|            + NetSalary() : double            |
+| + IncreaseSalary(percentage : double) : void |
+*/
