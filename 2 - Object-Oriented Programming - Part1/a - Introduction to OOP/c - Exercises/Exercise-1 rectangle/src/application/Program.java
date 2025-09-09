093705f919e6ac0ebbd65c4a6de263e141dea24e
Beatriz Almeida <beatriz.06.06.2016@gmail.com>
2025-09-08 21:56:25 -0300

    :sparkles: Add exercise 1

---
+++ 2 - Object-Oriented Programming - Part1/a - Introduction to OOP/c - Exercises/Exercise-1 rectangle/src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rectangle;
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Let's calculate the area, petimeter and diagonal of a rectangle.");
+		System.out.println("Enter the width and height of the triangle:");
+		
+		Rectangle rectangle = new Rectangle();
+		rectangle.height = sc.nextDouble();
+		rectangle.width = sc.nextDouble();
+		
+		System.out.printf("AREA = %.2f %n", rectangle.area());
+		System.out.printf("PERIMETER = %.2f %n", rectangle.perimeter());
+		System.out.printf("DIAGONAL = %.2f %n", rectangle.diagonal());
+		
+		sc.close();
+	}
+}
+/*
+|       Rectangle        |
+| ---------------------- |
+|    - Width : double    |
+|   - Height : double    |     
+| ---------------------- |
+|   + Area() : double    |
+| + Perimeter() : double |
+| + Diagonal() : double  |
+*/
---
+++ 2 - Object-Oriented Programming - Part1/a - Introduction to OOP/c - Exercises/Exercise-1 rectangle/src/entities/Rectangle.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Rectangle {
+
+	public double height;
+	public double width;
+	
+	
+	public double area() {
+		return height * width;
+	}
+	
+	public double perimeter() {
+		return height*2 + width*2;
+	}
+	
+	public double diagonal() {
+		double sumSquareLegs = Math.pow(height, 2) + Math.pow(width, 2);
+	return Math.sqrt(sumSquareLegs);
+	}
+}
