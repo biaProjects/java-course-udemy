8be6a89b2b643cd2e15216008c57998f55109946
Beatriz Almeida <beatriz.06.06.2016@gmail.com>
2025-09-17 09:11:48 -0300

    :sparkles: Add extra exercise

---
+++ 2 - Object-Oriented Programming - Part1/c - MemoryBehavior-arrays-lists/b - Vectors/Exercises/5 - Extra-hotel/src/application/Program.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.util.Scanner;
+
+import entities.Rent;
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		Rent[] rooms = new Rent[10];
+		
+		System.out.print("How many rooms will be rented? ");
+		int quantity = sc.nextInt();
+		
+		for (int i = 0; i < quantity; i++) {
+			Rent rent = new Rent();
+			
+			System.out.printf("Rent #%d %n", i + 1);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			rent.setName(name);
+			
+			System.out.print("Email: ");
+			String email = sc.next();
+			rent.setEmail(email);
+			
+			System.out.print("Room: ");
+			int room = sc.nextInt();
+			rent.setRoom(room);
+			
+			System.out.println();
+			
+			rooms[room] = rent;
+		}
+		
+		System.out.println("Busy rooms: ");
+		for (int i = 0; i < rooms.length; i += 1) {
+			if ( rooms[i] != null ) {
+				System.out.println(rooms[i]);
+			}
+		}
+		
+		sc.close();
+	}
+}
